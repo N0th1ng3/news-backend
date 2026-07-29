11905c5397163d37890cdc3282bc0794c22b27b3
N0th1ng3 <defront21@gmail.com>
2026-07-29 16:17:37 +0700

    Этап 2: Настроена SQLite база данных и защита от дубликатов

--- parser.py
+++ parser.py
@@ -1,7 +1,9 @@
+import sqlite3
+
 def fetch_news():
     print("Запуск сборщика новостей...")
     
-    # Имитируем ответ чужого API. Будто мы сделали запрос и получили такой JSON-список:
+    # Наш фейковый ответ от чужого API (две новости)
     mock_external_api_response = [
         {
             "id": 1,
@@ -21,7 +23,13 @@ def fetch_news():
         }
     ]
     
-    # Проходим циклом по каждой новости из полученного "ответа"
+    # Подключаемся к нашей созданной базе данных news.db
+    conn = sqlite3.connect("news.db")
+    cursor = conn.cursor()
+    
+    # Считаем, сколько новостей мы успешно добавим
+    added_count = 0
+    
     for item in mock_external_api_response:
         title = item.get('title')
         text = item.get('body')
@@ -29,13 +37,23 @@ def fetch_news():
         image_url = item.get('image')
         date = item.get('published_at')
         
-        # Печатаем всё в консоль строго по ТЗ
-        print("-" * 40)
-        print(f"ЗАГОЛОВОК: {title}")
-        print(f"ТЕКСТ: {text}")
-        print(f"КАРТИНКА: {image_url}")
-        print(f"ОРИГИНАЛ: {source_url}")
-        print(f"ДАТА: {date}")
+        try:
+            # Пробуем вставить строку в таблицу news
+            cursor.execute('''
+                INSERT INTO news (title, text, image_url, source_url, published_at)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (title, text, image_url, source_url, date))
+            added_count += 1
+            print(f"Успешно добавлена новость: {title}")
+            
+        except sqlite3.IntegrityError:
+            # Эта ошибка сработает, если UNIQUE заблокирует дубликат ссылки!
+            print(f"Новость уже есть в базе (дубликат пропущен): {title}")
+            
+    # Сохраняем изменения в файле базы данных и закрываем её
+    conn.commit()
+    conn.close()
+    print(f"Сборка завершена. Добавлено новых новостей: {added_count}")
 
 if __name__ == "__main__":
     fetch_news()
